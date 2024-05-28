a495f89e710945f8d8021e0756709de4fc071261
ValkarVarg <mikewilliamstfb@gmail.com>
2024-05-28 16:24:02 +0100

    refactor based on feedback - getTopics update, 404 error handler update, endpoint test update, getallarticles and get articlesbycomments test update

--- controllers/topics.controller.js
+++ controllers/topics.controller.js
@@ -4,7 +4,7 @@ const { fetchTopics, fetchEndpoints } = require("../models/topics.model");
 exports.getTopics = (req, res, next) => {
     fetchTopics()
     .then((topics) => {
-      res.status(200).send(topics)
+      res.status(200).send({topics})
     })
     .catch(next);
 };

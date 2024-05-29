564b25c72103bc726725d7ff23dbe7c4dd274288
ValkarVarg <mikewilliamstfb@gmail.com>
2024-05-29 17:02:25 +0100

    complete GET articles topic query

--- controllers/articles.controller.js
+++ controllers/articles.controller.js
@@ -18,7 +18,8 @@ exports.getArticle = (req, res, next) => {
 };
 
 exports.getAllArticles = (req, res, next) => {
-  fetchAllArticles()
+  const query = req.query
+  fetchAllArticles(query)
     .then((articles) => {
       res.status(200).send({ articles });
     })

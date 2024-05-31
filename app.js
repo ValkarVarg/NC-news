2a8b560ff210d83fe11d13b505d97d208a76d7f8
ValkarVarg <mikewilliamstfb@gmail.com>
2024-05-31 11:49:42 +0100

    converts to use routers to separate endpoints

--- app.js
+++ app.js
@@ -1,41 +1,35 @@
 const express = require("express");
+const apiRouter = require("./routes/api.router");
+const topicsRouter = require("./routes/topics.router");
+const articlesRouter = require("./routes/articles.router");
+const commentsRouter = require("./routes/comments.router"); 
+const usersRouter = require("./routes/users.router");
 
-
-const {getTopics, getEndpoints} = require("./controllers/topics.controller");
-const {getArticle, getAllArticles, getCommentsForArticle, postComment, patchArticle} = require("./controllers/articles.controller")
-const {deleteComment} = require("./controllers/comments.controller");
-const {getUsers} = require("./controllers/users.controller");
-
-const app = express()
+const app = express();
 
 app.use(express.json());
 
-app.get("/api", getEndpoints)
-app.get("/api/topics", getTopics);
 
-app.get("/api/articles/:articleId", getArticle)
-app.get("/api/articles", getAllArticles)
-app.get("/api/articles/:articleId/comments", getCommentsForArticle)
-app.get("/api/users", getUsers)
+app.use("/api", apiRouter);
+app.use("/api/topics", topicsRouter);
+app.use("/api/articles", articlesRouter);
+app.use("/api/comments", commentsRouter);
+app.use("/api/users", usersRouter);
 
-app.post("/api/articles/:articleId/comments", postComment)
-
-app.patch("/api/articles/:articleId", patchArticle)
-
-app.delete("/api/comments/:commentId", deleteComment)
 
 app.all('*', (req, res) => {
-  res.status(404).send({msg: "Route Not Found"})
-    })
+  res.status(404).send({ msg: "Route Not Found" });
+});
+
 
 app.use((err, req, res, next) => {
-    if (err.status && err.msg) {
-      res.status(err.status).send({ msg: err.msg });
-    } else if (err.code) {
-      res.status(400).send({ msg: "Bad Request" });
-    } else {
-      res.status(500).send({ msg: "Internal Server Error" });
-    }
-  });
+  if (err.status && err.msg) {
+    res.status(err.status).send({ msg: err.msg });
+  } else if (err.code) {
+    res.status(400).send({ msg: "Bad Request" });
+  } else {
+    res.status(500).send({ msg: "Internal Server Error" });
+  }
+});
 
 module.exports = app;
